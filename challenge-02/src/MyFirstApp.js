c6dfd806f0f1033f51fe3aae97a1eb2bd19c46bc
Nicolas Ramirez Arango <nicolas2000.24a@hotmail.com>
2023-09-28 19:28:06 -0500

    commit 2 challenge

---
+++ challenge-02/src/MyFirstApp.js
@@ -0,0 +1,10 @@
+const MyFirstApp = ({ title }) => {
+    return (
+      <>
+        <h1> { title } </h1>
+        <span> 10</span>
+    </>
+    )
+}
+
+export default MyFirstApp;
---
+++ challenge-02/src/index.js
@@ -0,0 +1,10 @@
+import App from './App';
+import MyFirstApp from './MyFirstApp';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot( document.getElementById("root")).render(
+  <React.StrictMode>
+      <MyFirstApp title= "My First App"/>
+  </React.StrictMode>
+)
