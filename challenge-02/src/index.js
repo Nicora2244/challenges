892bd5a183956d3260ccf99c41fa14e0871b98f7
Nicolas Ramirez Arango <nicolas2000.24a@hotmail.com>
2023-10-03 18:42:38 -0500

    commit challenge-4

--- challenge-02/src/index.js
+++ challenge-02/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MyFirstApp from './MyFirstApp';
+import ComponentApp from './ComponentApp';
 // import './index.css';
 // import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MyFirstApp value = {0}/>
+    <ComponentApp/>
   </React.StrictMode>
 );
